e8cadc84f0d5551fa1561f773036ae66940651aa
Ankit <ankit@ankits-mbp.in.ibm.com>
2020-03-06 11:37:25 +0530

    Updates

---
+++ precache-manifest.2dec0fe74c27aea6ba5067abb3f43694.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e9820cbdff1e96804321f50c8867765",
+    "url": "/Lyrical/index.html"
+  },
+  {
+    "revision": "a9c0fca19150f8140fc0",
+    "url": "/Lyrical/static/css/main.ffedc218.chunk.css"
+  },
+  {
+    "revision": "95a47b98aad741ebf0ff",
+    "url": "/Lyrical/static/js/2.e330792e.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/Lyrical/static/js/2.e330792e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9c0fca19150f8140fc0",
+    "url": "/Lyrical/static/js/main.3bf66c46.chunk.js"
+  },
+  {
+    "revision": "6eca53c4efcfde63cef5",
+    "url": "/Lyrical/static/js/runtime-main.3d51741e.js"
+  }
+]);
